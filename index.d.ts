a0f36d8c3f0fce46f0dc8c1fa7c527f762dcd689
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-09-05 13:05:36 +0530

    build fixed

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,56 @@
-import "./dist/FetchBuilder.d.ts";
+/// <reference types="node" />
+/// <reference types="node" />
+export declare function buildUrl(strings: TemplateStringsArray, ...p: any[]): string;
+export default class FetchBuilder {
+    private readonly request;
+    static get(url: any): FetchBuilder;
+    static put(url: any): FetchBuilder;
+    static post(url: any): FetchBuilder;
+    static delete(url: any): FetchBuilder;
+    static header(name: string, value: string): FetchBuilder;
+    static method(url: any, method: string): FetchBuilder;
+    private constructor();
+    log(logger: (...a: any[]) => void): FetchBuilder;
+    logWhenFailed(logger: (...a: any[]) => void): FetchBuilder;
+    get(url: any): void;
+    put(url: any): void;
+    post(url: any): void;
+    delete(url: any): void;
+    method(url: string, method: string): void;
+    signal(signal: AbortSignal): FetchBuilder;
+    form(name: string, value: string): FetchBuilder;
+    form(name: string, value: Blob, fileName: string): FetchBuilder;
+    jsonBody(body: any, encode?: boolean): FetchBuilder;
+    header(name: string, value: string): FetchBuilder;
+    path(name: string, value: any, encode?: boolean): FetchBuilder;
+    query(name: string, value: any, encode?: boolean): FetchBuilder;
+    asText(ensureSuccess?: boolean): Promise<string>;
+    asBlob(ensureSuccess?: boolean): Promise<import("buffer").Blob>;
+    asJson<T = any>(ensureSuccess?: boolean): Promise<unknown>;
+    asJsonResponse<T = any>(ensureSuccess?: boolean): Promise<{
+        result: Promise<unknown>;
+        headers: any;
+        status: number;
+    }>;
+    asTextResponse(ensureSuccess?: boolean): Promise<{
+        result: Promise<string>;
+        headers: any;
+        status: number;
+    }>;
+    asBlobResponse(ensureSuccess?: boolean): Promise<{
+        result: Promise<import("buffer").Blob>;
+        headers: any;
+        status: number;
+    }>;
+    execute<T>(ensureSuccess: boolean, postProcessor: (r: Response) => T): Promise<{
+        result: T;
+        headers: any;
+        status: number;
+    }>;
+    private append;
+}
+export declare class JsonError extends Error {
+    readonly json: any;
+    constructor(message: any, json: any);
+}
+//# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -1,2 +1,206 @@
-module.exports = require("./dist/FetchBuilder").default;
-module.exports.default = module.exports;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.JsonError = exports.buildUrl = void 0;
+function buildUrl(strings, ...p) {
+    let r = "";
+    for (let index = 0; index < strings.length; index++) {
+        const element = strings[index];
+        r += element;
+        if (index < p.length) {
+            r += encodeURIComponent(p[index]);
+        }
+    }
+    return r;
+}
+exports.buildUrl = buildUrl;
+class FetchBuilder {
+    static get(url) {
+        return this.method(url, "GET");
+    }
+    static put(url) {
+        return this.method(url, "PUT");
+    }
+    static post(url) {
+        return this.method(url, "POST");
+    }
+    static delete(url) {
+        return this.method(url, "DELETE");
+    }
+    static header(name, value) {
+        return new FetchBuilder({ headers: { url: "", method: "POST", [name]: value } });
+    }
+    static method(url, method) {
+        return new FetchBuilder({ url, method });
+    }
+    constructor(request) {
+        this.request = request;
+    }
+    log(logger) {
+        return this.append({ log: logger });
+    }
+    logWhenFailed(logger) {
+        return this.append({ logError: logger });
+    }
+    get(url) {
+        return this.method(url, "GET");
+    }
+    put(url) {
+        return this.method(url, "PUT");
+    }
+    post(url) {
+        return this.method(url, "POST");
+    }
+    delete(url) {
+        return this.method(url, "DELETE");
+    }
+    method(url, method) {
+        this.append({ url, method });
+    }
+    // public cancelToken(cancelToken: CancelToken) {
+    //     const ac = new AbortController();
+    //     cancelToken.registerForCancel(() => ac.abort());
+    //     return this.signal(ac.signal);
+    // }
+    signal(signal) {
+        return this.append({
+            signal
+        });
+    }
+    form(name, value, fileName) {
+        if (value === void 0) {
+            return this;
+        }
+        const body = this.request.body ?? new FormData();
+        if (fileName) {
+            if (typeof value === "string") {
+                throw new Error("value must be a blob with content type set correctly.");
+            }
+            body.append(name, value, fileName);
+        }
+        else {
+            body.append(name, value);
+        }
+        return this.append({ body });
+    }
+    jsonBody(body, encode = true) {
+        if (encode) {
+            body = JSON.stringify(body);
+        }
+        const headers = this.request.headers ?? {};
+        headers["content-type"] = "application/json";
+        return this.append({ body, headers });
+    }
+    header(name, value) {
+        const headers = this.request.headers ?? {};
+        headers[name] = value;
+        return this.append({ headers });
+    }
+    path(name, value, encode = true) {
+        let url = this.request.url;
+        if (encode) {
+            value = encodeURIComponent(value);
+        }
+        url = url.replace(name, value);
+        return this.append({ url });
+    }
+    query(name, value, encode = true) {
+        if (value === void 0) {
+            return this;
+        }
+        let url = this.request.url;
+        if (encode) {
+            value = encodeURIComponent(value);
+        }
+        name = encodeURIComponent(name);
+        if (url.indexOf("?") === -1) {
+            url += `?${name}=${value}`;
+        }
+        else {
+            url += `&${name}=${value}`;
+        }
+        return this.append({ url });
+    }
+    async asText(ensureSuccess = true) {
+        const { result } = await this.asTextResponse(ensureSuccess);
+        return result;
+    }
+    async asBlob(ensureSuccess = true) {
+        const { result } = await this.asBlobResponse(ensureSuccess);
+        return result;
+    }
+    async asJson(ensureSuccess = true) {
+        const { result } = await this.asJsonResponse(ensureSuccess);
+        return result;
+    }
+    async asJsonResponse(ensureSuccess = true) {
+        return this.execute(ensureSuccess, (x) => x.json());
+    }
+    async asTextResponse(ensureSuccess = true) {
+        return this.execute(ensureSuccess, (x) => x.text());
+    }
+    asBlobResponse(ensureSuccess = true) {
+        return this.execute(ensureSuccess, (x) => x.blob());
+    }
+    async execute(ensureSuccess = true, postProcessor) {
+        try {
+            const { headers, logError } = this.request;
+            let { log } = this.request;
+            const r = await fetch(this.request.url, this.request);
+            if (ensureSuccess) {
+                if (r.status > 300) {
+                    log = logError;
+                    log?.(`fetch: ${this.request.method ?? "GET"} ${this.request.url}`);
+                    if (log && headers) {
+                        for (const key in headers) {
+                            if ((Object.hasOwn && Object.hasOwn(headers, key)) || headers.hasOwnProperty(key)) {
+                                log?.(`${key}: ${headers[key]}`);
+                            }
+                        }
+                    }
+                    const type = r.headers.get("content-type");
+                    if (/\/json/i.test(type)) {
+                        const json = await r.json();
+                        const message = json.title
+                            ?? json.detail
+                            ?? json.message
+                            ?? json.exceptionMessage
+                            ?? "Json Server Error";
+                        throw new JsonError(message, json);
+                    }
+                    const text = await r.text();
+                    throw new Error(`Failed for ${this.request.url}\n${text}`);
+                }
+            }
+            log?.(`${this.request.method ?? "GET"} ${this.request.url}`);
+            if (log && headers) {
+                for (const key in headers) {
+                    if ((Object.hasOwn && Object.hasOwn(headers, key)) || headers.hasOwnProperty(key)) {
+                        log?.(`${key}: ${headers[key]}`);
+                    }
+                }
+            }
+            const result = await postProcessor(r);
+            if (log) {
+                log(`${r.status} ${r.statusText || "OK"}`);
+                log(result);
+            }
+            return { result, headers: r.headers, status: r.status };
+        }
+        catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+    append(r) {
+        return new FetchBuilder({ ...this.request, ...r });
+    }
+}
+exports.default = FetchBuilder;
+class JsonError extends Error {
+    constructor(message, json) {
+        super(message);
+        this.json = json;
+    }
+}
+exports.JsonError = JsonError;
+//# sourceMappingURL=index.js.map
